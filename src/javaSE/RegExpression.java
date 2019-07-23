76d0c49782ffcc3adf03e1c88f64887be049db61
850252329 <850252329@qq.com>
2019-07-23 12:07:12 +0800

    fixed

--- src/javaSE/RegExpression.java
+++ src/javaSE/RegExpression.java
@@ -3,6 +3,14 @@ package javaSE;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Java中的String类提供了支持正则表达式操作的方法，
+ * 包括：matches()、replaceAll()、replaceFirst()、
+ * split()。此外，Java中可以用Pattern类表示正则表达
+ * 式对象，它提供了丰富的API进行各种正则表达式操作
+ * @author abcd
+ *
+ */
 public class RegExpression {
 
 	public static void main(String[] args) {
