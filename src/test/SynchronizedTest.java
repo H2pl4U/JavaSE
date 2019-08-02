0b968199acf2ac7b36bd0d9669cc4b03294f8d92
850252329 <850252329@qq.com>
2019-08-02 21:45:18 +0800

    syn

--- src/test/SynchronizedTest.java
+++ src/test/SynchronizedTest.java
@@ -7,7 +7,7 @@ class SynchronizedExample {
 
     public void func1() {
         synchronized (this) {
-            for (int i = 0; i < 10; i++) {
+            for (int i = 0; i < 100; i++) {
                 System.out.print(i + " ");
             }
         }
@@ -18,9 +18,14 @@ public class SynchronizedTest {
 
 	public static void main(String[] args) {
 		SynchronizedExample e1 = new SynchronizedExample();
+		SynchronizedExample e2 = new SynchronizedExample();
 	    ExecutorService executorService = Executors.newCachedThreadPool();
 	    executorService.execute(() -> e1.func1());
 	    executorService.execute(() -> e1.func1());
+	    //0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 
+	    executorService.execute(() -> e1.func1());
+	    executorService.execute(() -> e2.func1());
+	    //0,1,0,1,2,3,4,2,3,4,5,6,7,5,6,7...½»Ìæ
 	}
 
 }
