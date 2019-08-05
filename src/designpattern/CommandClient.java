71c0eadb6b5dfc8136e56604bb4cb2ddab156426
850252329 <850252329@qq.com>
2019-08-05 21:58:33 +0800

    command

---
+++ src/designpattern/CommandClient.java
@@ -0,0 +1,94 @@
+package designpattern;
+
+/*
+ * 将命令封装成对象中，具有以下作用：
+ * 使用命令来参数化其它对象
+ * 将命令放入队列中进行排队
+ * 将命令的操作记录到日志中
+ * 支持可撤销的操作
+ * 
+ * 设计一个遥控器，可以控制电灯开关。
+ */
+//命令
+interface Command {
+    void execute();
+}
+
+//开灯
+class LightOnCommand implements Command {
+    Light light;
+
+    public LightOnCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.on();
+    }
+}
+//关灯
+class LightOffCommand implements Command {
+    Light light;
+
+    public LightOffCommand(Light light) {
+        this.light = light;
+    }
+
+    @Override
+    public void execute() {
+        light.off();
+    }
+}
+//灯
+class Light {
+
+    public void on() {
+        System.out.println("Light is on!");
+    }
+
+    public void off() {
+        System.out.println("Light is off!");
+    }
+}
+//遥控器
+class Invoker {
+    private Command[] onCommands;
+    private Command[] offCommands;
+    private final int slotNum = 7;
+
+    public Invoker() {
+        this.onCommands = new Command[slotNum];
+        this.offCommands = new Command[slotNum];
+    }
+
+    public void setOnCommand(Command command, int slot) {
+        onCommands[slot] = command;
+    }
+
+    public void setOffCommand(Command command, int slot) {
+        offCommands[slot] = command;
+    }
+
+    public void onButtonWasPushed(int slot) {
+        onCommands[slot].execute();
+    }
+
+    public void offButtonWasPushed(int slot) {
+        offCommands[slot].execute();
+    }
+}
+public class CommandClient {
+
+	public static void main(String[] args) {
+		Invoker invoker = new Invoker();
+        Light light = new Light();
+        Command lightOnCommand = new LightOnCommand(light);
+        Command lightOffCommand = new LightOffCommand(light);
+        invoker.setOnCommand(lightOnCommand, 0);
+        invoker.setOffCommand(lightOffCommand, 0);
+        invoker.onButtonWasPushed(0);
+        invoker.offButtonWasPushed(0);
+	}
+
+}
