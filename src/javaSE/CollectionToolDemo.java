918582719a8d552cbce634af14618d40a6a9d782
850252329 <850252329@qq.com>
2019-10-01 22:48:26 +0800

    cmt

--- src/javaSE/CollectionToolDemo.java
+++ src/javaSE/CollectionToolDemo.java
@@ -13,7 +13,7 @@ public class CollectionToolDemo {
 		list.add("8");
 		list.add("6");
 		// 集合元素排序
-		Collections.sort( list );
+		Collections.sort( list ); 
 		System.out.println(list);
 		//  集合元素存储位置打乱
 		//每次执行该方法，集合中存储的元素位置都会随机打乱
